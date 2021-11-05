e23fc78eaf8ed416c3abd4bed4d1f0413f29d4df
Aaron Minnick <abminnick@gmail.com>
2021-11-05 15:53:02 -0700

    site will let you open up ordered pizza for editing - however, bugs exist

--- js/scripts.js
+++ js/scripts.js
@@ -38,65 +38,113 @@ Pizza.prototype.calculatePrice = function() {
   return total;
 };
 
-function Topping(name, price) {
+function Topping(name, value, price) {
   this.name = name;
+  this.value = value;
   this.price = price;
 }
 
 //helper object to hold the list of all toppings
 function ListOfToppings() {
-  this.redsauce = new Topping("Classic Marinara", 0);
-  this.whitesauce = new Topping("Creamy Parmesan Alfredo", 0);
-  this.garlicoil = new Topping("Aglio e Olio", 0);
-  this.nosauce = new Topping("No Sauce", -1);
+  this.redsauce = new Topping("Classic Marinara", "redsauce", 0);
+  this.whitesauce = new Topping("Creamy Parmesan Alfredo", "whitesauce", 0);
+  this.garlicoil = new Topping("Aglio e Olio", "garlicoil", 0);
+  this.nosauce = new Topping("No Sauce", "nosauce", -1);
 
-  this.cheese = new Topping("Regular Cheese", 0);
-  this.doublecheese = new Topping("Double Cheese", 2);
-  this.vegancheese = new Topping("Vegan Cheese", 2);
-  this.nocheese = new Topping("No Cheese", -1.5);
+  this.cheese = new Topping("Regular Cheese", "cheese", 0);
+  this.doublecheese = new Topping("Double Cheese", "doublecheese", 2);
+  this.vegancheese = new Topping("Vegan Cheese", "vegancheese", 2);
+  this.nocheese = new Topping("No Cheese", "nocheese", -1.5);
 
-  this.pepperoni = new Topping("Pepperoni", 3);
-  this.sausage = new Topping("Italian Sausage", 3);
-  this.bacon = new Topping("Bacon", 3);
-  this.chicken = new Topping("Roast Chicken", 3);
-  this.anchovies = new Topping("Anchovies", 3);
-  this.veganpepperoni = new Topping("Vegan Pepperoni", 3);
-  this.vegansausage = new Topping("Vegan Sausage", 3);
+  this.pepperoni = new Topping("Pepperoni", "pepperoni", 3);
+  this.sausage = new Topping("Italian Sausage", "sausage", 3);
+  this.bacon = new Topping("Bacon", "bacon", 3);
+  this.chicken = new Topping("Roast Chicken", "chicken", 3);
+  this.anchovies = new Topping("Anchovies", "anchovies", 3);
+  this.veganpepperoni = new Topping("Vegan Pepperoni", "veganpepperoni", 3);
+  this.vegansausage = new Topping("Vegan Sausage", "vegansausage", 3);
 
-  this.artichokes = new Topping("Artichoke Hearts", 2.5);
-  this.peppers = new Topping("Bell Peppers", 2.5);
-  this.hotpeppers = new Topping("Cherry Peppers", 2.5);
-  this.blackolives = new Topping("Black Olives", 2.5);
-  this.mushrooms = new Topping("Mushrooms", 2.5);
-  this.onions = new Topping("Red Onions", 2.5);
-  this.garlicconfit = new Topping("Garlic Confit", 2.5);
-  this.kale = new Topping("Kale", 2.5);
-  this.potato = new Topping("Red Potato", 2.5);
+  this.artichokes = new Topping("Artichoke Hearts", "artichokes", 2.5);
+  this.peppers = new Topping("Bell Peppers", "peppers", 2.5);
+  this.hotpeppers = new Topping("Cherry Peppers", "hotpeppers", 2.5);
+  this.blackolives = new Topping("Black Olives", "blackolives", 2.5);
+  this.mushrooms = new Topping("Mushrooms", "mushrooms", 2.5);
+  this.onions = new Topping("Red Onions", "onions", 2.5);
+  this.garlicconfit = new Topping("Garlic Confit", "garlicconfit", 2.5);
+  this.kale = new Topping("Kale", "kale", 2.5);
+  this.potato = new Topping("Red Potato", "potato", 2.5);
+}
+
+//helper object to hold list of all ordered pizzas
+function ListOfOrderedPizzas() {
+  this.pizzas = {};
+  this.pizzaId = 1;
+}
+
+ListOfOrderedPizzas.prototype.addPizza = function(pizza) {
+  console.log(pizza.id);
+  if (typeof pizza.id === 'undefined') {  
+    pizza.id = this.pizzaId;
+    this.pizzaId += 1;
+    this.pizzas[pizza.id] = pizza;
+  }
 }
 
 //UI Logic
-function addOrderedPizza(pizza) {
-  let topList = "";
-  for (let t of pizza.toppings) {
-    topList += "<li>" + t.name + "</li>";
+function updateOrderedPiesDisplay(orderedPizzas) {
+  let htmlString = "";
+  for (let p in orderedPizzas.pizzas) { 
+    let topList = "";
+    pizza = orderedPizzas.pizzas[p];
+    for (let t of pizza.toppings) {
+      topList += "<li>" + t.name + "</li>";
+    }
+    htmlString += "<div class='row ordered-pie'><p>Size: " + pizza.size + "</p><p>Toppings:</p><ul>" + topList + "</ul><p>Price: <strong>$" + pizza.price + "</strong></p><button class='btn-dark' value='" + pizza.id + "'>Edit Order</button></div>";
   }
-  $("#ordered-pies").append("<div class='row'><p>Size: " + pizza.size + "</p><p>Toppings:</p><ul>" + topList + "</ul><p>Price: <strong>$" + pizza.price + "</strong></p></div>");
+  $("#ordered-pies").html(htmlString);
 }
 
+function addEditListeners(orderedPizzas) {
+  $("#ordered-pies").on('click', 'button', function() {
+    loadPriorPie(orderedPizzas, $(this).val());
+  });
+}
+
+function loadPriorPie(orderedPizzas, pizzaId) {
+  loadedPizza = orderedPizzas.pizzas[pizzaId];
+  //check proper size
+  $("#size-row input[value='" + loadedPizza.size + "']").prop('checked', true);
+  //uncheck all toppings, then check the ones the pizza has
+  $("#topping-row input").each(function() {
+    $(this).prop('checked', false);
+  });
+  for (let t of loadedPizza.toppings) {
+    $("#topping-row input[value='" + t.value + "']").prop('checked', true);
+  }
+  $("#order-picker").slideToggle(250);
+  $("#ordered-pies").slideToggle(250);
+  currentpizza = loadedPizza;
+}
 
 $(document).ready(function() {
   const listOfToppings = new ListOfToppings();
+  const orderedPizzas = new ListOfOrderedPizzas();
   const currentpizza = new Pizza('large');
+  addEditListeners(orderedPizzas);
   
   $("#pizza-maker").submit(function(event) {
     event.preventDefault();
-    $("#order-picker").slideToggle(250);
+    
+    currentpizza.size = $("#size-row input:checked").val();
     $("#topping-row input:checked").each(function(i) {
-      console.log(i +": "+ $(this).val());
       currentpizza.addTopping(listOfToppings[$(this).val()]);
     });
     currentpizza.calculatePrice();
-    addOrderedPizza(currentpizza);
+    orderedPizzas.addPizza(currentpizza);
+    updateOrderedPiesDisplay(orderedPizzas);
+
+    $("#order-picker").slideToggle(250);
+    $("#ordered-pies").slideToggle(250);
   });
 
 });
