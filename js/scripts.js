4d124c4784f1d1398258cd13adf9e405a68826fd
Aaron Minnick <abminnick@gmail.com>
2021-11-05 13:52:40 -0700

    add ListOfToppings object to hold all possible toppings

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Pizza(size) {
 }
 
 Pizza.prototype.addTopping = function(...topping) {
-  for (t of topping) {
+  for (let t of topping) {
     if (this.toppings.indexOf(t) === -1) {
     this.toppings.push(t);
     }
@@ -43,41 +43,39 @@ function Topping(name, price) {
   this.price = price;
 }
 
-function createToppings() {
-  return [
-  redsauce = new Topping("Classic Marinara", 0),
-  whitesauce = new Topping("Creamy Parmesan Alfredo", 0),
-  garlicoil = new Topping("Aglio e Olio", 0),
-  nosauce = new Topping("No Sauce", -1),
+//helper object to hold the list of all toppings
+function ListOfToppings() {
+  this.redsauce = new Topping("Classic Marinara", 0);
+  this.whitesauce = new Topping("Creamy Parmesan Alfredo", 0);
+  this.garlicoil = new Topping("Aglio e Olio", 0);
+  this.nosauce = new Topping("No Sauce", -1);
 
-  cheese = new Topping("Regular Cheese", 0),
-  doublecheese = new Topping("Double Cheese", 2),
-  vegancheese = new Topping("Vegan Cheese", 2),
-  nocheese = new Topping("No Cheese", -1.5),
+  this.cheese = new Topping("Regular Cheese", 0);
+  this.doublecheese = new Topping("Double Cheese", 2);
+  this.vegancheese = new Topping("Vegan Cheese", 2);
+  this.nocheese = new Topping("No Cheese", -1.5);
 
-  pepperoni = new Topping("Pepperoni", 3),
-  sausage = new Topping("Italian Sausage", 3),
-  bacon = new Topping("Bacon", 3),
-  chicken = new Topping("Roast Chicken", 3),
-  anchovies = new Topping("Anchovies", 3),
-  veganpepperoni = new Topping("Vegan Pepperoni", 3),
-  vegansausage = new Topping("Vegan Sausage", 3),
+  this.pepperoni = new Topping("Pepperoni", 3);
+  this.sausage = new Topping("Italian Sausage", 3);
+  this.bacon = new Topping("Bacon", 3);
+  this.chicken = new Topping("Roast Chicken", 3);
+  this.anchovies = new Topping("Anchovies", 3);
+  this.veganpepperoni = new Topping("Vegan Pepperoni", 3);
+  this.vegansausage = new Topping("Vegan Sausage", 3);
 
-  artichokes = new Topping("Artichoke Hearts", 2.5),
-  peppers = new Topping("Bell Peppers", 2.5),
-  hotpeppers = new Topping("Cherry Peppers", 2.5),
-  blackolives = new Topping("Black Olives", 2.5),
-  mushrooms = new Topping("Mushrooms", 2.5),
-  onions = new Topping("Red Onions", 2.5),
-  garlicconfit = new Topping("Garlic Confit", 2.5),
-  kale = new Topping("Kale", 2.5),
-  potato = new Topping("Red Potato", 2.5),
-  ]
+  this.artichokes = new Topping("Artichoke Hearts", 2.5);
+  this.peppers = new Topping("Bell Peppers", 2.5);
+  this.hotpeppers = new Topping("Cherry Peppers", 2.5);
+  this.blackolives = new Topping("Black Olives", 2.5);
+  this.mushrooms = new Topping("Mushrooms", 2.5);
+  this.onions = new Topping("Red Onions", 2.5);
+  this.garlicconfit = new Topping("Garlic Confit", 2.5);
+  this.kale = new Topping("Kale", 2.5);
+  this.potato = new Topping("Red Potato", 2.5);
 }
 
 //UI Logic
 $(document).ready(function() {
-  const possibleToppings = createToppings();
-  console.log(redsauce);
+  const listOfToppings = new ListOfToppings();
   
 });
